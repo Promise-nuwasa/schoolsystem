61bea3a44d06c0013e1d6b4245d0afbd8a6e4c45
esther ayebaza <estherayebaza@gmail.com>
2021-10-07 14:54:21 +0300

    add hosts

--- core/views.py
+++ core/views.py
@@ -1,15 +1,18 @@
-
-from cal.models import Event
-from cal.views import event
-from student.models import Student
 from django.shortcuts import render
-from courses.models import Courses
-from trainer.models import Trainer
-# Create your views here.
+
+# from cal.models import Event
+# from cal.views import event
+# from student.models import Student
+# from django.shortcuts import render
+# from courses.models import Courses
+# from trainer.models import Trainer
+# # Create your views here.
+# def home(request):
+#     students=Student.objects.count()
+#     courses=Courses.objects.count()
+#     trainers=Trainer.objects.count()
+#     event=Event.objects.count()
+#     data={"students":students,"courses":courses,"trainers":trainers,event:"event"}
+#     return render(request,"home.html",data)
 def home(request):
-    students=Student.objects.count()
-    courses=Courses.objects.count()
-    trainers=Trainer.objects.count()
-    event=Event.objects.count()
-    data={"students":students,"courses":courses,"trainers":trainers,event:"event"}
-    return render(request,"home.html",data)
+    return render(request, "home.html")
