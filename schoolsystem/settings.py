74ce4d9eeb8317e358d80c0494d11fa13f73c8eb
esther ayebaza <estherayebaza@gmail.com>
2021-10-07 13:32:05 +0300

    second commit

--- schoolsystem/settings.py
+++ schoolsystem/settings.py
@@ -141,6 +141,7 @@ STATICFILES_DIR=[
 
 MEDIA_URL="/images/"
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images/')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
